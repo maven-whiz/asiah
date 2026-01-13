095f54f8a982aa091734de0ed8a88f381c54c4fc
maven-whiz <mavenwhiz16@gmail.com>
2026-01-13 04:13:27 -1100

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,7 @@
+// let scroll = ScrollReveal({
+//     reset:true,
+//     duration:2600,
+//     distance:'60px'
+// })
+// Scroll.reveal('.image-transform', {delay:400, origin:'top'})
+// Scroll.reveal('.text-transform', {delay:300, origin:'left'})
